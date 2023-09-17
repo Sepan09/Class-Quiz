953b046fd45704dc6599d45eaf7a0c185d86d627
sepan mohammed <sepanmustafa@gmail.com>
2023-09-17 17:12:45 -0500

    final changes

--- highscore.js
+++ highscore.js
@@ -1,10 +1,16 @@
 var scoreRec = document.querySelector('.scored-1');
 var lastPage = document.querySelector('.go-back');
+var clearSb = document.querySelector(".clear-scores");
 
 
 var playerEl = JSON.parse(localStorage.getItem('highScores')) || [];
 
+function clearHistory () {
+    localStorage.clear();
+    scoreRec.textContent = '';
+}
 
+clearSb.addEventListener("click", clearHistory);
 
 // loop through playerEl array and create list item append to highscores.html
 
@@ -14,8 +20,13 @@ for (var i = 0; i < playerArr.length; i++) {
     var playerObj = playerArr[i];
 
     var btnPlayer = document.createElement("li");
-    btnPlayer.textContent = playerObj.initials, playerObj.score;
+    btnPlayer.textContent = playerObj.initials+"-"+playerObj.score;
     btnPlayer.setAttribute('class', "player-record")
     scoreRec.appendChild(btnPlayer);
 }
 
+function Previous() {
+
+}
+
+lastPage.addEventListener('click', Previous);

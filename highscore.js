4069337d678f38a4d5ba146220f8c2041bfdc5f2
sepan mohammed <sepanmustafa@gmail.com>
2023-09-17 12:18:02 -0500

    final touchups

--- highscore.js
+++ highscore.js
@@ -1,3 +1,21 @@
+var scoreRec = document.querySelector('.scored-1');
+var lastPage = document.querySelector('.go-back');
+
+
 var playerEl = JSON.parse(localStorage.getItem('highScores')) || [];
 
+
+
 // loop through playerEl array and create list item append to highscores.html
+
+var playerArr = playerEl;
+
+for (var i = 0; i < playerArr.length; i++) {
+    var playerObj = playerArr[i];
+
+    var btnPlayer = document.createElement("list");
+    btnPlayer.textContent = playerObj.text;
+    btnPlayer.setAttribute('class', "player-record")
+    scoreRec.appendChild(btnPlayer);
+}
+
